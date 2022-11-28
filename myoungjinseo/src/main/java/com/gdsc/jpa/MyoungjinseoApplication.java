f99d8064a3feb535b5f60bb077a6bb92ccb2570f
Seo, Myoungjin <sih5673@gmail.com>
2022-11-28 11:15:16 +0900

    feat Controller

--- myoungjinseo/src/main/java/com/gdsc/jpa/MyoungjinseoApplication.java
+++ myoungjinseo/src/main/java/com/gdsc/jpa/MyoungjinseoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication.
+
 public class JpaApplication {
 
     public static void main(String[] args) {
